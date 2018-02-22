1a58c90e31998ffad4b8364a928f790e9128e79f
pilik <pilikinp@me.com>
2018-02-22 15:13:34 +0300

    попытка реализации двухпоточности Сделать обработку входящих сообщений

--- models_client.py
+++ models_client.py
@@ -36,26 +36,28 @@ class Client():
         data = self.msg_client.msg(property, self.username)
         # print(data)
         self.socket.send(self.msg_client.pack(data))
+        self.rep = Repository(self.username)
         # msg_recv = self.socket.recv(1024)
         # msg_recv = self.msg_server.unpack(msg_recv)
         # print(msg_recv)
         #
-        # if msg_recv['response'] == '200':
-        #     print(msg_recv['alert'])
+        # time.sleep(5)
+        # if self.data['response'] == '200':
+        #     print(self.data['alert'])
         #     self.rep = Repository(self.username)
-        # elif msg_recv['response'] == '201':
-        #     print(msg_recv['alert'])
+        # elif self.data['response'] == '201':
+        #     print(self.data['alert'])
         #     print('Вы зарегистрировались под ником {}'.format(self.username))
         #     self.rep = Repository(self.username)
-        # elif msg_recv['response'] == '409':
-        #     print(msg_recv['alert'])
+        # elif self.data['response'] == '409':
+        #     print(self.data['alert'])
         #     sys.exit()
         # else:
-        #     print('Возникла ошибка {} обратитесь к справке или напишите в поддержку'.format(msg_recv['response']))
+        #     print('Возникла ошибка {} обратитесь к справке или напишите в поддержку'.format(self.data['response']))
         #     sys.exit()
 
-    def command(self,*args):
-        sock = args[0]
+    def command(self,sock):
+        # sock = args[0]
         print('Список комманд: \n1. получить/обновить список контактов \n2. написать сообщение \n'
               '3. написать сообщение в личку \n4. создать чат \n5. войти в чат \n'
               '6. получить список чатов \n7. добавить контакт \n8. удалить контакт\n9. выход')
@@ -130,8 +132,18 @@ class Client():
             sock.send(msg)
             time.sleep(0.5)
 
-    def _get_message(self, *args):
-        sock = args[0]
+    def _get_message(self, sock):
+        # sock = args[0]
+        # while True:
+        #     with self.lock:
+        #         data = sock.recv(1024)
+        #         time.sleep(0.1)
+        #     data = self.msg_server.unpack(data)
+        #     if 'action' in data:
+        #         print(data['time'], data['from'], data['message'])
+        #         self.rep.add_obj(HistoryMessage(data['time'], data['from'], data['to'], data['message']))
+        #     else:
+        #         print('ERROR ', data['response'], data['time'], data['alert'])
         while True:
             with self.lock:
                 data = sock.recv(1024)
@@ -141,24 +153,28 @@ class Client():
                 print(data['time'], data['from'], data['message'])
                 self.rep.add_obj(HistoryMessage(data['time'], data['from'], data['to'], data['message']))
             else:
-                print('ERROR ', data['response'], data['time'], data['alert'])
+                # print('ERROR ', data['response'], data['time'], data['alert'])
+                self.data = data
+                print (self.data)
 
     def msg(self, *args):
         sock = args[0]
-        t1 = threading.Thread(target=self._send_message, args=(sock, 'msg'))
+        self._send_message(sock,'msg')
+        # t1 = threading.Thread(target=self._send_message, args=(sock, 'msg'))
         # t2 = threading.Thread(target=self._get_message, args= (sock,))
-        t1.start()
+        # t1.start()
         # t2.start()
-        t1.join()
+        # t1.join()
         # t2.join()
 
     def msg_to(self, *args):
         sock = args[0]
-        t1 = threading.Thread(target=self._send_message, args=(sock,'msg_to'))
+        self._send_message(sock,'msg_to')
+        # t1 = threading.Thread(target=self._send_message, args=(sock,'msg_to'))
         # t2 = threading.Thread(target=self._get_message, args= (sock,))
-        t1.start()
+        # t1.start()
         # t2.start()
-        t1.join()
+        # t1.join()
         # t2.join()
 
     def login_chat(self, *args):
@@ -171,11 +187,12 @@ class Client():
 
     def chat(self, *args):
         sock = args[0]
-        t1 = threading.Thread(target=self._send_message, args=(sock, 'chat'))
+        self._send_message(sock, 'chat')
+        # t1 = threading.Thread(target=self._send_message, args=(sock, 'chat'))
         # t2 = threading.Thread(target=self._get_message, args=(sock,))
-        t1.start()
+        # t1.start()
         # t2.start()
-        t1.join()
+        # t1.join()
         # t2.join()
 
     def run(self):
@@ -193,12 +210,11 @@ class Client():
                 print('неверная команда')
                 sys.exit()
             while True:
-                t3 = threading.Thread(target=self._get_message, args= (sock,))
-                t2 = threading.Thread(target=self.command(), args=(sock,))
+                t1 = threading.Thread(target=self._get_message, args= (sock,))
+                t2 = threading.Thread(target=self.command, args=(sock,))
+                t1.start()
                 t2.start()
+                t1.join()
                 t2.join()
-                t3.start()
-
-                t3.join()
 
     # добавить поток который будет постоянно слушать сервер
